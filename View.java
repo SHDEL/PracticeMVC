5ccc6a9475422baacd4892a700a5537118c74080
satfey <wwawazerrr@gmail.com>
2025-02-02 22:23:51 +0700

    test commit

--- View.java
+++ View.java
@@ -8,7 +8,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.border.Border;
+
 
 public class View {
     JFrame  frame;
